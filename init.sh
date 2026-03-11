b290a833fbf780873237d284d196bf2587233d74
Carson <jackparadise520a@gmail.com>
2026-03-11 11:40:34 +0800

    初始專案: 添加要的依賴項

---
+++ init.sh
@@ -0,0 +1,11 @@
+cargo new crontab
+
+cargo add tokio -F full
+cargo add chrono
+cargo add cron
+cargo add anyhow
+cargo add log
+cargo add env_logger
+cargo add serde -F derive
+cargo add serde_json
+
